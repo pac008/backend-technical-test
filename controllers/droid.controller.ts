f9bedbd201b5bffdc597b156b90d7dcf0a54bc84
pac008 <herreraalcantarmiguelangel@gmail.com>
2021-09-03 10:01:10 +0200

    functions async

--- controllers/droid.controller.ts
+++ controllers/droid.controller.ts
@@ -3,7 +3,7 @@ import { Droid, Scan, Coordinates } from '../interfaces/droid.interface';
 
 
 
-export const targetCoordinates = ( req: Request, res: Response ) => {
+export const targetCoordinates = async ( req: Request, res: Response ) => {
   
   try {
     let newScan:Scan[] = []
@@ -17,11 +17,11 @@ export const targetCoordinates = ( req: Request, res: Response ) => {
       });
     }
     if ( body.protocols.length == 1) {
-      newScan = protocol( body.protocols[0], body.scan );
+      newScan = await  protocol( body.protocols[0], body.scan );
     }
 
     if( body.protocols.length > 1) {
-      newScan = typeOfProtocol(body.protocols, body.scan);
+      newScan = await typeOfProtocol(body.protocols, body.scan);
     }
     newScan.forEach( (item: Scan ) => coordinates.push( item.coordinates ) );
     
@@ -34,56 +34,65 @@ export const targetCoordinates = ( req: Request, res: Response ) => {
   }
 } 
 
-const protocol = ( protocol: string, scan: Scan[]):Scan[] => {
+const protocol = async ( protocol: string, scan: Scan[] ):Promise<Scan[]> => {
   
-  const newScan:Scan[]  = [];
-  switch (protocol) {
-    case 'avoid-crossfire':
-      scan.forEach( (item: Scan) => {
-        if ( !item.allies ) {
-        newScan.push( item )
+  try {
+    const newScan: Scan[] = [];
+
+    switch (protocol) {
+      case 'avoid-crossfire':
+        scan.forEach((item: Scan) => {
+          if (!item.allies) {
+            newScan.push(item)
+          }
+        })
+        break;
+      case 'assist-allies':
+        scan.forEach((item: Scan) => {
+          if (item.allies) {
+            newScan.push(item)
+          }
+        })
+        break;
+      case 'furthest-enemies':
+        scan.sort((a: Scan, b: Scan) => (b.coordinates.x + b.coordinates.y) - (a.coordinates.x + a.coordinates.y));
+        scan.forEach((item: Scan) => newScan.push(item));
+        break;
+      case 'closest-enemies':
+        scan.sort((a: Scan, b: Scan) => (a.coordinates.x + a.coordinates.y) - (b.coordinates.x + b.coordinates.y));
+        scan.forEach((item: Scan) => newScan.push(item))
+        break;
+      case 'prioritize-mech':
+        const existMech = scan.find((item: Scan) => item.enemies.type == 'mech');
+        if (existMech) {
+          newScan.push(existMech)
+        } else {
+          newScan.push(scan[0]);
         }
-      })
-    break;
-    case 'assist-allies':
-      scan.forEach( (item: Scan) => {
-        if ( item.allies ) {
-        newScan.push( item )
-        }
-      })
-    break;
-    case 'furthest-enemies':
-      scan.sort( (a, b) => (b.coordinates.x + b.coordinates.y ) - ( a.coordinates.x + a.coordinates.y ) );
-      scan.forEach( (item: Scan) =>  newScan.push( item ) );
-    break;  
-    case 'closest-enemies':
-      scan.sort( (a:Scan, b:Scan) => ( a.coordinates.x + a.coordinates.y ) - (b.coordinates.x + b.coordinates.y ) );
-      scan.forEach( (item: Scan) => newScan.push( item ) )
-    break;
-    case 'prioritize-mech':
-      const existMech = scan.find( (item: Scan) => item.enemies.type == 'mech');
-      if ( existMech ) {
-        newScan.push( existMech )
-      } else {
-        newScan.push( scan[0] );
-      }
-    break;
-    case 'avoid-mech':
-      scan.forEach( (item: Scan) => {
-        if ( item.enemies.type != 'mech' ) {
-          newScan.push( item )
-        }
-      })
-    break;
+        break;
+      case 'avoid-mech':
+        scan.forEach((item: Scan) => {
+          if (item.enemies.type != 'mech') {
+            newScan.push(item)
+          }
+        })
+        break;
+
+    }
+    return newScan;
+
+  } catch (error) {
+    console.log(error);
+    throw new Error("something unexpected happened");
+
   }
-      return newScan;
 }
 
-const typeOfProtocol = ( protocols: string[], scan:Scan[] ): Scan[] => {
+const typeOfProtocol = async ( protocols: string[], scan:Scan[] ):Promise<Scan[]> => {
   let newScan: Scan[] = scan;
 
-  for (let i = 0; i < protocols.length; i++ ) {
-    newScan = protocol( protocols[i], newScan );
+  for (let i = 0; i < protocols.length; i++) {
+     newScan = await protocol( protocols[i], newScan );
   }
   return newScan;
 }
